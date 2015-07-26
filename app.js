763fa8e07314c300f2017bcb8594689071923047
Tiffany Han <hantiffany@gmail.com>
2015-07-26 11:32:28 -0700

    Initialize project.

---
+++ app.js
@@ -0,0 +1,33 @@
+/* --------------------- Model Data ---------------------- */
+
+/*
+var location = {
+	lat: 34.040536,
+	lng: -118.236646
+};
+
+var mapOptions = {
+	center: new google.maps.LatLng(location.lat, location.lng),
+	zoom: 15,
+	mapTypeId: google.maps.MapTypeId.ROADMAP
+}
+
+
+*/
+/* --------------------- ViewModel ----------------------*/
+/*
+var ViewModel = function() {
+	var self = this;
+
+	this.initialize = function() {
+		var mapCanvas = document.getElementById('map-canvas');
+		var map = new google.maps.Map(mapCanvas, mapOptions);
+	};
+
+	google.maps.event.addDomListener(window, 'load', this.initialize);
+};
+
+ko.applyBindings(new ViewModel());
+
+*/
+
