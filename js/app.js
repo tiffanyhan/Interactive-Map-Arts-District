74d01061a159d5b7282aa1b71fa4a4fbefb35400
Tiffany Han <hantiffany@gmail.com>
2015-08-07 15:29:37 -0700

    Perform query using knockout observable instead of jquery.

--- js/app.js
+++ js/app.js
@@ -25,49 +25,56 @@ var Model = {
 			name: 'Wurstkuche',
 			street: '800 E 3rd St',
 			linkName: 'wurstkuche.com',
-			phone: '(213) 687-4444'
+			phone: '(213) 687-4444',
+			keywords: ['Wurstkuche', 'food']
 		},
 		{
 			coordinates: {lat: 34.041979, lng: -118.235406},
 			name: 'Urth Caffe',
 			street: '451 S Hewitt St',
 			linkName: 'urthcaffe.com',
-			phone: '(213) 797-4534'
+			phone: '(213) 797-4534',
+			keywords: ['Urth Caffe', 'coffee', 'cafe']
 		},
 		{
 			coordinates: {lat: 34.045406, lng: -118.237441},
 			name: 'EightyTwo',
 			street: '707 E 4th Pl',
 			linkName: 'eightytwo.la',
-			phone: '(213) 626-8200'
+			phone: '(213) 626-8200',
+			keywords: ['EightyTwo', 'arcade', 'bar', 'drinks']
 		},
 		{
 			coordinates: {lat: 34.045161, lng: -118.238748},
 			name: 'X Lanes',
 			street: '333 Alameda St #300',
 			linkName: 'xlanesla.com',
-			phone: '(213) 229-8910'
+			phone: '(213) 229-8910',
+			keywords: ['X Lanes', 'bowling']
 		},
 		{
 			coordinates: {lat: 34.046143, lng: -118.234074},
 			name: 'Eat Drink Americano',
 			street: '923 E 3rd St #101',
 			linkName: 'eatdrinkamericano.com',
-			phone: '(213) 620-0781'
+			phone: '(213) 620-0781',
+			keywords: ['Eat Drink Americano', 'food']
 		},
 		{
 			coordinates: {lat: 34.045414, lng: -118.236251},
 			name: 'The Pie Hole',
 			street: '714 Traction Ave',
 			linkName: 'thepieholela.com',
-			phone: '(213) 537-0115'
+			phone: '(213) 537-0115',
+			keywords: ['The Pie Hole', 'food']
 		},
 		{
 			coordinates: {lat: 34.045279, lng: -118.238533},
 			name: 'Shojin',
 			street: '333 Alameda St #310',
 			linkName: 'theshojin.com',
-			phone: '(213) 617-0305'
+			phone: '(213) 617-0305',
+			keywords: ['Shojin', 'food', 'sushi', 'japanese']
 		}
 	],
 
@@ -99,8 +106,7 @@ var ViewModel = function() {
 	self.query = ko.observable('');
 
 	self.search = function() {
-		var searchInput = $('.search-box').val().trim();
-		var searchValue = new RegExp(searchInput, 'i');
+		var searchValue = new RegExp(self.query(), 'i');
 
 		var locationsListLength = self.locationsList().length;
 		var i;
